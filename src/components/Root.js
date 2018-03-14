80624270f4262a0782be72ffc9b12e0eea3cac20
Koen Romers <info@koenromers.com>
2018-03-14 16:11:29 +0000

    feat(home): setup home with current production html and styling

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { ConnectedRouter } from 'react-router-redux';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import Home from './Home';
 
 function Root(props) {
   const { store, history } = props;
   return (
     <Provider store={store}>
       <ConnectedRouter history={history}>
-        <div className="container">
-          foo
+        <div id="wrapper">
+          <div id="content">
+            <Route path="/" component={Home} />
+          </div>
         </div>
       </ConnectedRouter>
     </Provider>
