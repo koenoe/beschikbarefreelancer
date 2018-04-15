a3f45ec7649a95a48fabc4782948b8c69eb3efd3
Koen Romers <info@koenromers.com>
2018-04-15 22:21:14 +0200

    tests(): company actions

--- src/test-setup.js
+++ src/test-setup.js
@@ -1,6 +1,12 @@
 // @flow
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import { fetch as mockFetch } from 'jest-fetch-mock';
+
+declare var global: { fetch: any }
+
+const globalMockFetch: Promise<> = mockFetch;
+global.fetch = globalMockFetch;
 
 const options: Object = {
   adapter: new Adapter(),
--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -1,25 +1,18 @@
 // @flow
 export const callApi: Function = (url: string, options: RequestOptions) =>
   fetch(url, options)
-    .then(
-      (response: Response) => {
-        if (!response.ok) {
-          return Promise.reject(response.statusText);
-        }
-        const contentType: string = response.headers.get('Content-Type') || '';
-        if (contentType.indexOf('json') !== -1) {
-          return response.json();
-        } else if (contentType.indexOf('text') !== -1) {
-          return response.text();
-        }
-        return response;
-      },
-      error => Promise.reject(error),
-    )
-    .then(
-      result => ({ result }: any),
-      error => ({ error }: any),
-    )
-    .catch(error => ({ error }: any));
+    .then((response: Response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      const contentType: string = response.headers.get('Content-Type') || '';
+      if (contentType.indexOf('json') !== -1) {
+        return response.json();
+      } else if (contentType.indexOf('text') !== -1) {
+        return response.text();
+      }
+      return response;
+    })
+    .then(result => ({ result }: any));
 
 export default callApi;
